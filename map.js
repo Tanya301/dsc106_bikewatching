e84b8ac6a7fa12b529c9c10bf3b04766bd7ec8a2
Tanya301 <tanya@samokhvalov.com>
2025-02-20 17:00:19 -0800

    MAAAAAP (fr tho)

---
+++ map.js
@@ -0,0 +1,30 @@
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoidGFueWFzYW0iLCJhIjoiY203ZHp1NGZ3MDhuNzJrcHFidml2bnF6YSJ9.6Y4hKjnY_Rpz_KWLsoWrPQ';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+    container: 'map', // ID of the div where the map will render
+    style: 'mapbox://styles/mapbox/streets-v12', // Map style
+    center: [-71.09415, 42.36027], // [longitude, latitude]
+    zoom: 12, // Initial zoom level
+    minZoom: 5, // Minimum allowed zoom
+    maxZoom: 18 // Maximum allowed zoom
+});
+
+map.on('load', () => { 
+    map.addSource('boston_route', {
+        type: 'geojson',
+        data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+    });
+    map.addLayer({
+        id: 'bike-lanes',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': '#32D400',  // A bright green using hex code
+            'line-width': 5,          // Thicker lines
+            'line-opacity': 0.6       // Slightly less transparent
+        }
+    });
+
+});
